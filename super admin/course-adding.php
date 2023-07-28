8b48527a988e563c40b5a005881ca1cbd08860bc
viveknhc <viveknhc@gmail.com>
2023-07-28 14:13:29 +0530

    27/07/23

--- super admin/course-adding.php
+++ super admin/course-adding.php
@@ -14,10 +14,11 @@
                         <h4 class="fw-semibold mb-8">Course Adding</h4>
                         <nav aria-label="breadcrumb">
                             <ol class="breadcrumb">
-                              <li class="breadcrumb-item"><a class="text-muted text-decoration-none" href="index">Dashboard</a></li>
-                              <li class="breadcrumb-item" aria-current="page">Course adding</li>
+                                <li class="breadcrumb-item"><a class="text-muted text-decoration-none"
+                                        href="index">Dashboard</a></li>
+                                <li class="breadcrumb-item" aria-current="page">Course adding</li>
                             </ol>
-                          </nav>
+                        </nav>
                     </div>
                     <div class="col-3">
                         <div class="text-center mb-n5">
@@ -34,67 +35,60 @@
         <div class="card">
             <div class="card-body">
 
-<div class="row">
-    <div class="col-lg-6">
-        <form>
-            <div class="row">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <form>
+                            <div class="row">
 
-                <div class="col-lg-6 pb-3">
-                    <div class="form-group">
-                        <label class="form-label">Upload Video</label>
-                        <input type="url" class="form-control">
+                                <div class="col-lg-6 pb-3">
+                                    <div class="form-group">
+                                        <label class="form-label">Upload Video</label>
+                                        <input type="url" class="form-control">
+                                    </div>
+                                </div>
+
+
+                                <div class="col-lg-6 pb-3">
+                                    <div class="form-group">
+                                        <label class="form-label">Catagory</label>
+                                        <select class="select2 form-control select2-hidden-accessible"
+                                            style="width: 100%; height: 36px" data-select2-id="select2-data-1-nn1h"
+                                            tabindex="-1" aria-hidden="true">
+                                            <option data-select2-id="select2-data-3-bxds">Section 1</option>
+                                            <option data-select2-id="select2-data-3-bxds">Section 2</option>
+                                            <option data-select2-id="select2-data-3-bxds">Section 3</option>
+                                            <option data-select2-id="select2-data-3-bxds">Section 4</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-lg-6 pb-3">
+                                    <div class="form-group">
+                                        <label class="form-label">Video Class name</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-6 pb-3">
+                                    <div class="form-group">
+                                        <label class="form-label">Description</label>
+                                        <input type="text" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <button class="btn btn-primary">Upload</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-lg-6">
+
+                        <div>
+                            <img src="assets/images/add-user.png" alt="">
+                        </div>
                     </div>
                 </div>
 
 
-                <div class="col-lg-6 pb-3">
-                    <div class="form-group">
-                        <label class="form-label">Catagory</label>
-
-                        <select class="select2 form-control select2-hidden-accessible"
-                            style="width: 100%; height: 36px" data-select2-id="select2-data-1-nn1h"
-                            tabindex="-1" aria-hidden="true">
-                            <option data-select2-id="select2-data-3-bxds">Section 1</option>
-                            <option data-select2-id="select2-data-3-bxds">Section 2</option>
-                            <option data-select2-id="select2-data-3-bxds">Section 3</option>
-                            <option data-select2-id="select2-data-3-bxds">Section 4</option>
-
-
-
-
-                        </select>
-
-                    </div>
-                </div>
-
-                <div class="col-lg-6 pb-3">
-                    <div class="form-group">
-                        <label class="form-label">Video Class name</label>
-                        <input type="text" class="form-control">
-                    </div>
-                </div>
-
-                <div class="col-lg-6 pb-3">
-                    <div class="form-group">
-                        <label class="form-label">Description</label>
-                        <input type="text" class="form-control">
-                    </div>
-                </div>
-            </div>
-            <div class="text-center">
-                <button class="btn btn-primary">Upload</button>
-            </div>
-        </form>
-    </div>
-    <div class="col-lg-6">
-
-        <div>
-            <img src="assets/images/add-user.png" alt="">
-        </div>
-    </div>
-</div>
-
-           
 
             </div>
         </div>
